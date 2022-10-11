350cb9ec7987f79a2ac712811a1411bbbe686b3f
relkimm <bbonggu92@gmail.com>
2022-10-12 00:04:46 +0900

    feat: todo 완료 css 적용

--- src/todo/list/components/TodoItem.jsx
+++ src/todo/list/components/TodoItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DeleteButton from "./DeleteButton";
-import styles from "../style.module.css";
 import CompleteButton from "./CompleteButton";
+import styles from "../style.module.css";
 
 export default function TodoItem({ todo }) {
+  const completedStyle = todo.completed ? styles.completed : "";
+
   return (
     <li className={styles.todoItem}>
-      <p className={styles.todoText}>{todo.text}</p>
+      <p className={`${styles.todoText} ${completedStyle}`}>{todo.text}</p>
       <div>
         <CompleteButton id={todo.id} />
         <DeleteButton id={todo.id} />
